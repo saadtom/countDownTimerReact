9f529f4ebe6319ade861364d3bfa3f6f61b3138e
Tommy Tomaye <tommytomaye2018@gmail.com>
2018-09-22 19:56:52 -0700

    Count down react timer by Tommy

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline : 'December 25, 2018',
+      newDeadline : ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({
+      deadline : this.state.newDeadline
+    });
+  }
+  render() {
+   return (
+    <div className="App">
+     <div className="App-title">Count Down to {this.state.deadline}</div>
+     <Clock deadline={this.state.deadline}/>
+     <div>
+      <input type="text"
+             placeholder="new date"
+             onChange = {event => this.setState({newDeadline : event.target.value})}/>
+      <button onClick={() => this.changeDeadline() }>Submit</button>
+     </div>
+    </div>
+   )
+  }
+}
+
+export default App
